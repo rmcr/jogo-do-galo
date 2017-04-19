b3e9f4c9a99c0cad494705d7fba965d7a559598e
Rita <rmcrdeveloper@gmail.com>
2017-04-19 21:20:55 +0100

    finalizacaodojogo

--- main.js
+++ main.js
@@ -1,21 +1,96 @@
-var celulas;
+	var cells;
 
-var jogador="azul";
+	var player="azul";
 
-var azul="azul";
+	var azul="azul";
 
+	var amarelo="amarelo";
 
-var amarelo="amarelo";
+	
+	// var coluna = celula.attr('x');
+	// var linha = celula.parent().attr('y');
 
-function teste(celula){
-	var coluna = celula.attr('x');
-	var linha = celula.parent().attr('y');
+	//creat a function to check if player won
+function teste(events){
+	var win = true;
 
-	for(i=0; i<celulas; i++){
-		$('td').hasClass(jogador)
-			if()
+	//verificar horizontais
+	for(var line=0; line<cells; line++){
+		win = true;
+		for(var col=0; col<cells; col++){
+			if(!$('tr[data-y='+ line +']> td[data-x='+ col +']').hasClass(player)){
+				win = false;
+				break;
+			}
+		}
+		if(win==true){
+			break;
+		}
 	}
-};
+
+	if(win==true){
+		setTimeout(function(){
+			alert("Vencedor");
+		}, 100);
+		return;
+	}
+
+	//verificar verticais
+	for(var col=0; col<cells; col++){
+		win=true;
+		for(var line=0; line<cells; line++){
+			if(!$('tr[data-y='+ line +']> td[data-x='+ col +']').hasClass(player)){
+				win = false;
+				break;
+			}
+		}
+		if(win==true){
+			break;
+		}
+	}
+	if(win==true){
+		setTimeout(function(){
+			alert("Vencedor");
+		}, 100);
+		return;
+	}
+
+	//verificar diagonais
+	
+	win=true;
+	for(batatas=0; batatas<cells; batatas++){
+		if(!$('tr[data-y='+ batatas +'] > td[data-x='+ batatas +']').hasClass(player)){
+			win = false;
+			break;
+		}
+	}
+
+	
+	if(win==true){
+		setTimeout(function(){
+			alert("Vencedor");
+		}, 100);
+		return;
+	}
+
+	//as outras diagonais
+
+	win=true;
+	for(batatas=0; batatas<cells; batatas++){
+		var conta = cells-1-batatas;
+		if(!$('tr[data-y='+ conta +'] > td[data-x='+ batatas +']').hasClass(player)){
+			win = false;
+			break;
+		}
+	}
+
+	if(win==true){
+		setTimeout(function(){
+			alert("Vencedor");
+		}, 100);
+		return;
+	}
+}
 
 $(document).ready(function(){
 	$.ajax({
@@ -34,36 +109,31 @@ $(document).ready(function(){
 
 		success: function(data, code, xhl){ 
 			
-			celulas=data;
+			cells=data;
 
-			for(var i = 0; i < celulas; i++ ){
-				$('table').append('<tr y="'+i+'"></tr>')
+			for(var i = 0; i < cells; i++ ){
+				$('table').append('<tr data-y="'+i+'"></tr>')
 			}
 
-			for(var i = 0; i < celulas; i++){
-				$('tr').append('<td x="'+i+'"></td>')
+			for(var i = 0; i < cells; i++){
+				$('tr').append('<td data-x="'+i+'"></td>')
 			}
 		
 
-		$("td").click(function(ev){
+		$("td").click(function(event){
 			/*console.log($(ev.currentTarget));*/
-			$(ev.currentTarget).addClass(jogador);
+			$(event.currentTarget).addClass(player);
 			
-			teste($(ev.currentTarget));
+			teste($(event.currentTarget));
 
-			if(jogador===azul){
-				jogador=amarelo
+			if(player===azul){
+				player=amarelo;
 			} 
 
 			else{
-				jogador=azul
+				player=azul;
 			}
 
-		})
-
-
-		}
-	})
-
-})
-
+		});
+	}});
+});
